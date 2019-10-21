28b99ebf65201763214abd7663967e6b43279ac2
Derik Breedt <dbreedt@retrorabbit.co.za>
2019-10-21 16:51:42 +0200

    some more tuning

--- setup_pg.sh
+++ setup_pg.sh
@@ -8,6 +8,7 @@ set -e
   create extension IF NOT EXISTS postgis_topology;
 EOSQL
 
+# values from pgtune 5GB RAM + SDD + WebApplication
 echo <<-EOF
 fsync=off
 synchronous_commit=off
@@ -16,16 +17,17 @@ wal_level=minimal
 max_worker_processes=2
 max_parallel_workers_per_gather=1
 max_parallel_workers=2
-work_mem=90MB
-shared_buffers=2GB
-effective_cache_size=5GB
+work_mem=60MB
+max_connections=100
+shared_buffers=1280MB
+effective_cache_size=3840MB
 max_wal_senders=0
 archive_mode=off
-maintenance_work_mem=500MB
+maintenance_work_mem=100MB
 checkpoint_completion_target=0.7
 wal_buffers=16MB
 random_page_cost=1.1
-effective_io_concurrency=300
+effective_io_concurrency=200
 min_wal_size=1GB
 max_wal_size=2GB
 EOF >> /var/lib/postgresql/data/postgresql.conf
