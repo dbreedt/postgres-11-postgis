cae4fefdbf413511b2f076a83a3473428622bc04
Derik Breedt <dbreedt@retrorabbit.co.za>
2019-10-21 16:30:30 +0200

    Added some tuning to make the db run faster

--- setup_pg.sh
+++ setup_pg.sh
@@ -7,3 +7,25 @@ set -e
   create extension IF NOT EXISTS postgis;
   create extension IF NOT EXISTS postgis_topology;
 EOSQL
+
+echo <<-EOF
+fsync=off
+synchronous_commit=off
+full_page_writes=off
+wal_level=minimal 
+max_worker_processes=2
+max_parallel_workers_per_gather=1
+max_parallel_workers=2
+work_mem=90MB
+shared_buffers=2GB
+effective_cache_size=5GB
+max_wal_senders=0
+archive_mode=off
+maintenance_work_mem=500MB
+checkpoint_completion_target=0.7
+wal_buffers=16MB
+random_page_cost=1.1
+effective_io_concurrency=300
+min_wal_size=1GB
+max_wal_size=2GB
+EOF >> /var/lib/postgresql/data/postgresql.conf
