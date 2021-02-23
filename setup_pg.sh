3bd9e68db02d1c4f0d2234628e8420486384a6d1
Derik Breedt <dbreedt@retrorabbit.co.za>
2021-02-23 11:33:55 +0200

    Updated memory back to 2GB and changed the type of system to oltp - added max_worker lines

--- setup_pg.sh
+++ setup_pg.sh
@@ -8,22 +8,25 @@ set -e
   create extension IF NOT EXISTS postgis_topology;
 EOSQL
 
-# values from pgtune 1GB RAM + 2CPU + 20connections + SDD + WebApplication
+# values from pgtune 1GB RAM + 2CPU + 20connections + SDD + Oltp
 
 echo fsync=off >> /var/lib/postgresql/data/postgresql.conf
 echo synchronous_commit=off >> /var/lib/postgresql/data/postgresql.conf
 echo full_page_writes=off >> /var/lib/postgresql/data/postgresql.conf
-echo work_mem=20MB >> /var/lib/postgresql/data/postgresql.conf
+echo work_mem=30MB >> /var/lib/postgresql/data/postgresql.conf
 echo max_connections=20 >> /var/lib/postgresql/data/postgresql.conf
-echo shared_buffers=256MB >> /var/lib/postgresql/data/postgresql.conf
-echo effective_cache_size=768MB >> /var/lib/postgresql/data/postgresql.conf
+echo shared_buffers=512MB >> /var/lib/postgresql/data/postgresql.conf
+echo effective_cache_size=1536MB >> /var/lib/postgresql/data/postgresql.conf
 echo archive_mode=off >> /var/lib/postgresql/data/postgresql.conf
 echo maintenance_work_mem=128MB >> /var/lib/postgresql/data/postgresql.conf
-echo checkpoint_completion_target=0.7 >> /var/lib/postgresql/data/postgresql.conf
+echo checkpoint_completion_target=0.9 >> /var/lib/postgresql/data/postgresql.conf
 echo random_page_cost=1.1 >> /var/lib/postgresql/data/postgresql.conf
 echo effective_io_concurrency=200 >> /var/lib/postgresql/data/postgresql.conf
-echo max_wal_senders=0 >> /var/lib/postgresql/data/postgresql.conf
 echo wal_level=minimal >> /var/lib/postgresql/data/postgresql.conf
 echo wal_buffers=16MB >> /var/lib/postgresql/data/postgresql.conf
-echo min_wal_size=1GB >> /var/lib/postgresql/data/postgresql.conf
-echo max_wal_size=4GB >> /var/lib/postgresql/data/postgresql.conf
+echo min_wal_size=2GB >> /var/lib/postgresql/data/postgresql.conf
+echo max_wal_size=8GB >> /var/lib/postgresql/data/postgresql.conf
+echo max_worker_processes=2 >> /var/lib/postgresql/data/postgresql.conf
+echo max_parallel_workers_per_gather=1 >> /var/lib/postgresql/data/postgresql.conf
+echo max_parallel_workers=2 >> /var/lib/postgresql/data/postgresql.conf
+echo max_parallel_maintenance_workers=1 >> /var/lib/postgresql/data/postgresql.conf
