37a96a9cd4c6eb6b36f2a4c46dbde2b6360133ce
Derik Breedt <dbreedt@retrorabbit.co.za>
2021-04-08 14:32:10 +0200

    Updated ram to 4GB

--- setup_pg.sh
+++ setup_pg.sh
@@ -8,17 +8,17 @@ set -e
   create extension IF NOT EXISTS postgis_topology;
 EOSQL
 
-# values from pgtune 2GB RAM + 2CPU + 20connections + SDD + Oltp
+# values from pgtune 4GB RAM + 2CPU + 100connections + SDD + Oltp
 
 echo fsync=off >> /var/lib/postgresql/data/postgresql.conf
 echo synchronous_commit=off >> /var/lib/postgresql/data/postgresql.conf
 echo full_page_writes=off >> /var/lib/postgresql/data/postgresql.conf
 echo work_mem=30MB >> /var/lib/postgresql/data/postgresql.conf
 echo max_connections=100 >> /var/lib/postgresql/data/postgresql.conf
-echo shared_buffers=512MB >> /var/lib/postgresql/data/postgresql.conf
-echo effective_cache_size=1536MB >> /var/lib/postgresql/data/postgresql.conf
+echo shared_buffers=1GB >> /var/lib/postgresql/data/postgresql.conf
+echo effective_cache_size=3GB >> /var/lib/postgresql/data/postgresql.conf
 echo archive_mode=off >> /var/lib/postgresql/data/postgresql.conf
-echo maintenance_work_mem=128MB >> /var/lib/postgresql/data/postgresql.conf
+echo maintenance_work_mem=256MB >> /var/lib/postgresql/data/postgresql.conf
 echo checkpoint_completion_target=0.9 >> /var/lib/postgresql/data/postgresql.conf
 echo random_page_cost=1.1 >> /var/lib/postgresql/data/postgresql.conf
 echo effective_io_concurrency=200 >> /var/lib/postgresql/data/postgresql.conf
